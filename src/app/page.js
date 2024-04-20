496e0dcd50eb77bfc8f98d8b2275bd9415a62578
vasiliaskid <vasiliaskid@gmail.com>
2024-04-20 15:43:18 +0700

    change page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <button className="btn btn-primary">Submit</button>
-    </div>
-  );
+  return <div>Sedang dikerjainnnn</div>;
 }
